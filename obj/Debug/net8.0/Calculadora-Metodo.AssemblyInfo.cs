8a7cde6f43de864c9732d1c189afe0285d22e3ec
vitoria <0001065043@senaimgaluno.com.br>
2025-02-04 16:39:14 -0300

    novos cs

--- obj/Debug/net8.0/Calculadora-Metodo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculadora-Metodo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora-Metodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a91ff5be8da314e4f6cef6669206dbaabe6fbf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e256e6e3caf82cb3daecf0ea30099c9f809e8511")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora-Metodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora-Metodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
